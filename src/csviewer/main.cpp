6e165c7b9fa87df02fe367392e4f291e85b81501
Stepan Shabalin <stomperhomp@gmail.com>
2024-02-13 16:55:18 -0500

    Initial commit

--- src/csviewer/main.cpp
+++ src/csviewer/main.cpp
@@ -31,15 +31,12 @@
 
 int main(int argc, char *argv[])
 { 
-    QApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
 
     // initialize log
     CSLogger logger;
     logger.setLogRootDir(LOG_ROOT_DIR);
     logger.setLogPrefix(LOG_PREFIX);
-
-    bool suc = QObject::connect(&app, &QApplication::aboutToQuit, &logger, &CSLogger::onAboutToQuit, Qt::DirectConnection);
-    Q_ASSERT(suc);
 
     logger.initialize();
 
@@ -49,18 +46,4 @@ int main(int argc, char *argv[])
     qInfo() << "Applicaion Name : "     << APP_NAME;
     qInfo() << "Applicaion Version : "  << APP_VERSION;
     qInfo() << "****************************************";
-
-    cs::CSApplication::getInstance()->start();
-    ViewerWindow w;
-    w.show();
-
-    // set style and font
-    QString stylePath = QString("%1/themes/global.css").arg(APP_PATH);
-    QString fontPath = QString("%1/fonts/SourceHanSansCN-Regular.ttf").arg(APP_PATH);
-
-    qInfo() << "set style and font";
-    CommonHelper::setStyle(stylePath);
-    CommonHelper::setFont(fontPath);
-    
-    return app.exec();
 }
